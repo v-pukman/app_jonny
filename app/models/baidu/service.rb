9ac011d04eafc85970767d2543c882748bf662f2
Victor Pukman <victor.pukman@gmail.com>
2016-09-17 20:35:37 +0800

    app parser tests added

--- app/models/baidu/service.rb
+++ app/models/baidu/service.rb
@@ -4,72 +4,27 @@ class Baidu::Service
     @api = Baidu::ApiClient.new
   end
 
-  def save_app preview_info_source, additional_data={}
-    preview_info_source = JSON.parse(preview_info_source.to_json)
-    itemdata = preview_info_source['itemdata']
-    if itemdata.nil? || itemdata['docid'].nil?
+  # preview_info example - fixtures/static/baidu/preview_info_source
+  def save_from_preview_info preview_info, additional_data={}
+    preview_info = JSON.parse(preview_info.to_json)
+    full_info = nil
+    itemdata = preview_info['itemdata']
+    if itemdata.nil? || !itemdata.is_a?(Hash) || itemdata['docid'].nil?
       return nil
     end
 
     id_str = Baidu::App.build_id_str(itemdata['type'], itemdata['packageid'], itemdata['groupid'], itemdata['docid'])
-
-    p "checking #{id_str}"
     app = Baidu::App.where(id_str: id_str).first
-    # create if doesn't exit
-    full_info = nil
     if app.nil?
-      full_info = api.get(:app, docid: itemdata['docid'])
-
-      data = JSON.parse(full_info.to_json)['result']['data']['base_info']
-      full_data = JSON.parse(full_info.to_json)['result']['data']['base_info']
-
-      attrs = data.keep_if {|a| Baidu::App.column_names.include?(a.to_s)}
-      attrs['today_str_download'] = full_data['today_strDownload']
-      attrs['now_download'] = full_data['nowDownload']
-      attrs['app_type'] = full_data['type']
-
-      #attrs['search_position'] = search_position if search_position
-      #attrs['in_board_position'] = in_board_position if in_board_position
-      #attrs['baidu_game_board_id'] = additional_data[:baidu_game_board_id] if additional_data[:baidu_game_board_id]
-      #attrs['q_app_id'] = additional_data[:q_app_id] if additional_data[:q_app_id]
-      #attrs['rank_position'] = additional_data[:rank_position] if additional_data[:rank_position]
-
-      #if full_data['dev_display']
-      #  attrs['dev_id'] = full_data['dev_display']['dev_id']
-      #  attrs['dev_name'] = full_data['dev_display']['dev_name']
-      #  attrs['dev_score'] = full_data['dev_display']['dev_score']
-      #  attrs['dev_level'] = full_data['dev_display']['dev_level']
-      #end
-
-      # rebuild id_str using full data
-      # all fields must be filled
-      id_str = Baidu::App.build_id_str(attrs['app_type'], attrs['packageid'], attrs['groupid'], attrs['docid'])
-
-      begin
-        # check for second time
-        # because sometimes preview info doesnt have all id
-        p "checking2 #{id_str}"
-        app = Baidu::App.where(id_str: id_str).first
-        if app.nil?
-          app = Baidu::App.new(attrs)
-          app.save!
-          p "app created #{app.id}"
-        else
-          app.update_attributes(attrs)
-          p "app was updated instead of create: baidu_app_id #{app.id}"
-        end
-      # handle db dublication error
-      rescue ActiveRecord::RecordNotUnique => e
-        app = Baidu::App.where(id_str: id_str).first
-        app.update_attributes(attrs)
-        p "app was updated instead of create on not_uniq error: baidu_app_id #{app.id}"
-      end
-
-    else
-      p "app already created"
+      full_info = api.get :app, docid: itemdata['docid']
+      attrs = full_info_to_attrs full_info
+      # will check for second time
+      # because sometimes preview info doesnt have all id
+      # also it handles db record dublication error
+      app = create_or_update attrs
     end
 
-    #save_game_day(app.id, preview_info_source, full_info, search_position, in_board_position)
+    #save_game_day(app.id, preview_info, full_info, search_position, in_board_position)
 
     # additional_data
     # developer
@@ -85,5 +40,45 @@ class Baidu::Service
     app
   end
 
+  def create_or_update attrs
+    id_str = Baidu::App.build_id_str(attrs['app_type'], attrs['packageid'], attrs['groupid'], attrs['docid'])
+    app = Baidu::App.where(id_str: id_str).first
+    if app.nil?
+      app = Baidu::App.new(attrs)
+      app.save!
+    else
+      app.update_attributes(attrs)
+    end
+    app
+  rescue ActiveRecord::RecordNotUnique
+    app = Baidu::App.where(id_str: id_str).first
+    app.update_attributes(attrs)
+    app
+  end
+
+  def full_info_to_attrs full_info
+    data = JSON.parse(full_info.to_json)['result']['data']['base_info']
+    full_data = JSON.parse(full_info.to_json)['result']['data']['base_info']
+
+    attrs = data.keep_if {|a| Baidu::App.column_names.include?(a.to_s)}
+    attrs['today_str_download'] = full_data['today_strDownload']
+    attrs['now_download'] = full_data['nowDownload']
+    attrs['app_type'] = full_data['type']
+
+    #attrs['search_position'] = search_position if search_position
+    #attrs['in_board_position'] = in_board_position if in_board_position
+    #attrs['baidu_game_board_id'] = additional_data[:baidu_game_board_id] if additional_data[:baidu_game_board_id]
+    #attrs['q_app_id'] = additional_data[:q_app_id] if additional_data[:q_app_id]
+    #attrs['rank_position'] = additional_data[:rank_position] if additional_data[:rank_position]
+
+    #if full_data['dev_display']
+    #  attrs['dev_id'] = full_data['dev_display']['dev_id']
+    #  attrs['dev_name'] = full_data['dev_display']['dev_name']
+    #  attrs['dev_score'] = full_data['dev_display']['dev_score']
+    #  attrs['dev_level'] = full_data['dev_display']['dev_level']
+    #end
+
+    attrs
+  end
 
 end
--- spec/models/baidu/service_spec.rb
+++ spec/models/baidu/service_spec.rb
@@ -10,42 +10,58 @@ RSpec.describe Baidu::Service do
   it { expect(preview_info_source).to_not eq nil }
   it { expect(service.api.is_a?(Baidu::ApiClient)).to eq true }
 
-  describe "#save_app" do
-    let(:data) { full_info_source['result']['data']['base_info'] }
-    context "when app is not saved in db" do
-      it "saves new app" do
-        VCR.use_cassette("baidu/get_app--for-save-app") do
-          app = service.save_app(preview_info_source)
-          expect(app.persisted?).to eq true
-        end
-      end
-      it "fixes wrong keys" do
-        VCR.use_cassette("baidu/get_app--for-save-app") do
-          app = service.save_app(preview_info_source)
-          expect(app.today_str_download.to_s).to eq data['today_strDownload']
-          expect(app.now_download.to_s).to eq data['nowDownload']
-          expect(app.app_type).to eq data['type']
-        end
-      end
-      # sometimes group or packageid can be blank
-      # in this case app didnt found in first time
-      # but after full info downloaded must be found and updated
-      it "check existing of app again after full_info loading" do
-        VCR.use_cassette("baidu/get_app--for-save-app") do
-          app = create :baidu_app, app_type: data['type'], packageid: data['packageid'], groupid: data['groupid'], docid: data['docid']
-          expect(app.persisted?).to eq true
-          expect(app.sname).to eq nil
 
-          preview_info_source['itemdata'].delete('groupid')
-          new_app = service.save_app(preview_info_source)
+  describe "#create_or_update" do
+    let(:attrs) do
+      service.full_info_to_attrs(full_info_source)
+    end
+    let(:id_str) { Baidu::App.build_id_str(attrs['app_type'], attrs['packageid'], attrs['groupid'], attrs['docid']) }
+    it "creates new app" do
+      app = service.create_or_update(attrs)
+      expect(app.persisted?).to eq true
+    end
+    it "updates created app" do
+      app = create :baidu_app, attrs
+      new_brief = 'awesome game!'
+      attrs['brief'] = new_brief
+      service.create_or_update(attrs)
+      expect(app.reload.brief).to eq new_brief
+    end
+    it "handles ActiveRecord::RecordNotUnique error and updates record" do
+      app = create :baidu_app, attrs
+      apps = [app]
+      allow(apps).to receive(:first).and_return(app)
+      allow(Baidu::App).to receive(:where).and_return(apps)
+      allow(app).to receive(:nil?).and_raise(ActiveRecord::RecordNotUnique, "error")
+      expect(app).to receive(:update_attributes).with(attrs)
+      service.create_or_update(attrs)
+    end
+  end
 
-          expect(new_app.id).to eq app.id
-          expect(new_app.sname).to eq data['sname']
-        end
+  describe "#save_from_preview_info" do
+    let(:itemdata) { preview_info_source['itemdata'] }
+    let(:id_str) { Baidu::App.build_id_str(itemdata['type'], itemdata['packageid'], itemdata['groupid'], itemdata['docid']) }
+    it "return nil when no docid" do
+      preview_info_source['itemdata'].delete('docid')
+      app = service.save_from_preview_info preview_info_source
+      expect(app).to eq nil
+    end
+    it "return nil when itemdata is not Hash" do
+      preview_info_source['itemdata'] = []
+      app = service.save_from_preview_info preview_info_source
+      expect(app).to eq nil
+    end
+    it "calls create_or_update when app is not created" do
+      VCR.use_cassette("baidu/get_app--for-save-app") do
+        expect(service).to receive(:create_or_update)
+        service.save_from_preview_info preview_info_source
       end
-
-      it "updated record when RecordNotUnique db error raised" do
-      end
+    end
+    it "doesnt request data when app exist" do
+      app = create :baidu_app, app_type: itemdata['type'], packageid: itemdata['packageid'], groupid: itemdata['groupid'], docid: itemdata['docid']
+      expect(app.persisted?).to eq true
+      expect(service).to_not receive(:create_or_update)
+      service.save_from_preview_info preview_info_source
     end
   end
 end
