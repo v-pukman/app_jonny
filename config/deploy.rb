c27646c03abb0f9af2b354376f053b502e6a6885
Victor Pukman <victor.pukman@gmail.com>
2016-12-05 00:05:08 +0800

    remove bin folder from linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, '/home/deploy/app_jonny'
 # set :pty, true
 
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
