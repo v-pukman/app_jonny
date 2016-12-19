4833dc6f361bff5477e515a1ed20442054821376
Victor Pukman <victor.pukman@gmail.com>
2016-12-19 17:58:14 +0800

    deploy fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,21 +1,8 @@
-every 1.day, at: '00:05' do
+every 1.day, at: '17:05' do
   rake 'baidu:send_report'
 end
 
-# every 1.day, at: '00:10' do
-#   rake 'baidu:download_apps_from_boards'
-# end
-
-# every 1.day, at: '18:05' do
-#   rake 'baidu:download_soft_ranks'
-#   rake 'baidu:download_game_ranks'
-#   rake 'baidu:download_crown_ranks'
-# end
-
-
-
-
-every 1.day, at: '01:00' do
+every 1.day, at: '18:00' do
   rake 'baidu:download_apps_from_boards' #~1hour
   rake 'baidu:download_soft_ranks' #~15min
   rake 'baidu:download_game_ranks'
